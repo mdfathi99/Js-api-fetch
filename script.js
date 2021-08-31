b42f44c3104bc0953ea5c757f2c5ea9cd7453229
mdfathi99 <md.fathi.1999@gmail.com>
2021-09-01 01:03:47 +0600

    Added API source

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+fetch ('https://api.punkapi.com/v2/beers')
+.then(function (response) {
+    return response.json();
+})
+.then(function (data) {
+    appendData(data);
+})
+.catch(function (err) {
+    console.log('error: ' + err);
+});
 function appendData(data) {
     var mainContainer = document.getElementById("news1");
     for (var i = 0; i < data.length; i++) {
