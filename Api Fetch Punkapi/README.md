c5031d77442b294efe164a763cacc84d2e5a9c0e
mdfathi99 <md.fathi.1999@gmail.com>
2021-09-08 01:15:56 +0600

    Changed Path

---
+++ Api Fetch Punkapi/README.md
@@ -0,0 +1,1 @@
+# Js-api-fetch
--- Api Fetch Punkapi/script.js
+++ Api Fetch Punkapi/script.js
@@ -13,9 +13,9 @@ function appendData(data) {
     for (var i = 0; i < data.length; i++) {
         mainContainer.innerHTML += `
             <div class="news">
-              <img src=${data[i]["image_url"]} />
+              <img class="img" src=${data[i]["image_url"]} />
               <h1 class="heading">${data[i].name}</h1>
-              <p class="excerpt">${data[i].description.slice(0, 100)}</p>
+              <p class="excerpt">${data[i].description}</p>
             </div>
           `
     }
